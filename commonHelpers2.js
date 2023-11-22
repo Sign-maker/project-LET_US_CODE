89a6dba085f6fb93e45cf3ab0a099fbdffe4545a
Sign-maker <136499347+Sign-maker@users.noreply.github.com>
2023-11-22 21:11:44 +0000

    Deploying to gh-pages from @ Sign-maker/project-LET_US_CODE@fcfb9f98872b7c9f425952865e98a09d87333b66 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,22 +1,37 @@
-import"./assets/styles-f648ceb5.js";import{a}from"./assets/vendor-a61d8330.js";a.defaults.baseURL="https://food-boutique.b.goit.study/api";class d{constructor(){}async getProductCategories(){const{data:s}=await a("/products/categories");return s}async getProducts(s){const{data:o}=await a("/products",{params:{...s}});return o}async getProductDetails(s){const{data:o}=await a(`/products/${s}`);return o}async getPopularProducts(){const{data:s}=await a("/products/popular");return s}async getDiscountedProducts(){const{data:s}=await a("/products/discount");return s}async subscribeToNewsletter(s){return await a.post("/subscription",s,{headers:{"Content-Type":"application/json"}})}async placeOrder(s){const{data:o}=await a.post("/orders",s,{headers:{"Content-Type":"application/json"}});return o}}const c=new d;async function m(){try{const t=await c.getProductCategories();console.log("productCategories",t);const s={keyword:"apple",category:"Fresh_Produce",byABC:!0,byPrice:!0,byPopularity:!0,page:1,limit:6},o=await c.getProducts(s);console.log("products",o);const e="640c2dd963a319ea671e3676",r=await c.getProductDetails(e);console.log("productsDetails",r);const n=await c.getPopularProducts();console.log("popularProducts",n);const i=await c.getDiscountedProducts();console.log("discountedProducts",i);const u={email:"test@gmail.com",products:[{productId:"640c2dd963a319ea671e383b",amount:2}]},l=await c.placeOrder(u);console.log("placeOrder",l);const g={email:"test@gmail.com"},b=await c.subscribeToNewsletter(g);console.log("subscribeToNewsletter",b)}catch{}finally{}}m();const p=new d,y=document.querySelector(".popular-list"),P=document.querySelector(".discount-list");async function h(){try{const t=await p.getPopularProducts();y.innerHTML=v(t)}catch{}}h();async function w(){try{const t=await p.getDiscountedProducts();P.innerHTML=f(t)}catch{}}w();function v(t){return t.map(s=>{const{_id:o,name:e,img:r,category:n,size:i,popularity:u}=s;return`<li class="popular-item" id="${o}">
+import"./assets/styles-ef998177.js";import{a as c}from"./assets/vendor-a61d8330.js";c.defaults.baseURL="https://food-boutique.b.goit.study/api";class u{constructor(){}async getProductCategories(){const{data:t}=await c("/products/categories");return t}async getProducts(t){const{data:e}=await c("/products",{params:{...t}});return e}async getProductDetails(t){const{data:e}=await c(`/products/${t}`);return e}async getPopularProducts(){const{data:t}=await c("/products/popular");return t}async getDiscountedProducts(){const{data:t}=await c("/products/discount");return t}async subscribeToNewsletter(t){return await c.post("/subscription",t,{headers:{"Content-Type":"application/json"}})}async placeOrder(t){const{data:e}=await c.post("/orders",t,{headers:{"Content-Type":"application/json"}});return e}}const d=new u;async function y(){try{const s=await d.getProductCategories();console.log("productCategories",s);const t={keyword:"apple",category:"Fresh_Produce",byABC:!0,byPrice:!0,byPopularity:!0,page:1,limit:6},e=await d.getProducts(t);console.log("products",e);const o="640c2dd963a319ea671e3676",a=await d.getProductDetails(o);console.log("productsDetails",a);const r=await d.getPopularProducts();console.log("popularProducts",r);const i=await d.getDiscountedProducts();console.log("discountedProducts",i);const n={email:"test@gmail.com",products:[{productId:"640c2dd963a319ea671e383b",amount:2}]},p=await d.placeOrder(n);console.log("placeOrder",p);const g={email:"test@gmail.com"},m=await d.subscribeToNewsletter(g);console.log("subscribeToNewsletter",m)}catch{}finally{}}y();const f={page:1,perPage:9,totalPages:60,results:[{_id:"640c2dd963a319ea671e383b",name:"Ackee",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e383b.png",category:"Fresh_Produce",price:8.99,size:"16 oz",is10PercentOff:!1,popularity:8},{_id:"640c2dd963a319ea671e3860",name:"Allspice",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3860.png",category:"Pantry_Items",price:2.99,size:"1.5oz",is10PercentOff:!1,popularity:8},{_id:"640c2dd963a319ea671e3861",name:"Almond Extract",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3861.png",category:"Pantry_Items",price:4.99,size:"2oz",is10PercentOff:!1,popularity:0},{_id:"640c2dd963a319ea671e385f",name:"Almond Milk",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e385f.png",category:"Beverages",price:3.99,size:"1L",is10PercentOff:!1,popularity:0},{_id:"640c2dd963a319ea671e3814",name:"Almonds",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3814.png",category:"Pantry_Items",price:8.99,size:"16 oz bag",is10PercentOff:!1,popularity:0},{_id:"640c2dd963a319ea671e385e",name:"Ancho Chillies",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e385e.png",category:"Pantry_Items",price:4.99,size:"100g",is10PercentOff:!1,popularity:0},{_id:"640c2dd963a319ea671e3865",name:"Anchovy Fillet",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3865.png",category:"Meat_&_Seafood",price:3.49,size:"2oz",is10PercentOff:!1,popularity:0},{_id:"640c2dd963a319ea671e3660",name:"Apple Cider Vinegar",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3660.png",price:6.99,size:"500 ml",category:"Pantry_Items",is10PercentOff:!1,popularity:0},{_id:"640c2dd963a319ea671e3825",name:"Apples",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3825.png",category:"Fresh_Produce",price:1.5,size:"1 lb",is10PercentOff:!0,popularity:0}]},h=f.results,P=document.querySelector(".product-card-list");P.innerHTML=b(h);function b(s){return s.map(({category:t,img:e,is10PercentOff:o,name:a,popularity:r,price:i,size:n,_id:p})=>`<li class="products-card-item">
+                <a class="products-card-link" href="#"><img class="product-image" src="${e}" alt="${a}" /></a>
+                <a class="products-card-link" href="#"><h3 class="product-name">${a}</h3></a>
+                <ul class="product-description-list">
+                        <li><p class="product-text">Category:<span class="product-text-black"> ${t}</span>Size:<span class="product-text-black"> ${n}</span></p></li>
+                        <li><p class="product-text product-text-popular">Popularity:<span class="product-text-black"> ${r}</span></p></li>
+                </ul>
+                <div class="wrap-prise-and-btn">
+                    <h3 class="price">$${i}</h3>
+                    <button class="btn btn-shopping-cart js-add-btn">
+                        <svg class="icon-shopping-cart" width="18" height="18">
+                            <use href="./icons.svg#icon-shopping-cart"></use>
+                        </svg>
+                    </button>
+                </div>
+            </li>`).join("")}const l=new u,v=document.querySelector(".popular-list"),w=document.querySelector(".discount-list");async function _(){try{const s=await l.getPopularProducts();v.innerHTML=z(s)}catch{}}_();async function $(){try{const s=await l.getDiscountedProducts();w.innerHTML=k(s)}catch{}}$();function z(s){return s.map(t=>{const{_id:e,name:o,img:a,category:r,size:i,popularity:n}=t;return`<li class="popular-item" id="${e}">
         <div class="popular-img-wrapper">
           <img
-            src="${r}"
-            alt="${e}"
+            src="${a}"
+            alt="${o}"
             class="popular-img"
           />
         </div>
         <div class="popular-desc-wrapper">
-          <h3 class="popular-desc-name">${e}</h3>
+          <h3 class="popular-desc-name">${o}</h3>
           <h4 class="popular-desc-text">
-            Category: <span class="popular-desc-span">${n}</span>
+            Category: <span class="popular-desc-span">${r}</span>
           </h4>
           <div class="popular-desc-inner">
             <h4 class="popular-desc-text">
               Size: <span class="popular-desc-span">${i}</span>
             </h4>
             <h4 class="popular-desc-text">
-              Popularity: <span class="popular-desc-span">${u}</span>
+              Popularity: <span class="popular-desc-span">${n}</span>
             </h4>
           </div>
         </div>
@@ -36,16 +51,16 @@ import"./assets/styles-f648ceb5.js";import{a}from"./assets/vendor-a61d8330.js";a
             ></use>
           </svg>
         </button>
-      </li>`}).join("")}function f(t){return t.slice(0,2).map(o=>{const{_id:e,name:r,img:n,price:i}=o;return`<li class="discount-item" id="${e}">
+      </li>`}).join("")}function k(s){return s.slice(0,2).map(e=>{const{_id:o,name:a,img:r,price:i}=e;return`<li class="discount-item" id="${o}">
         <div class="discount-img-wrapper">
           <img
-            src="${n}"
-            alt="${r}"
+            src="${r}"
+            alt="${a}"
             class="discount-img"
           />
         </div>
         <div class="discount-desc-wrapper">
-          <p class="discount-desc-text">${r}</p>
+          <p class="discount-desc-text">${a}</p>
           <div class="discount-box">
             <p class="discount-desc-text">$${i}</p>
             <button
