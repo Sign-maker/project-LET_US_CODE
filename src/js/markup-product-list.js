fcfb9f98872b7c9f425952865e98a09d87333b66
Sign-maker <muratovdv76@gmail.com>
2023-11-22 23:11:24 +0200

    markUp corrected

--- src/js/markup-product-list.js
+++ src/js/markup-product-list.js
@@ -1,9 +1,103 @@
 import { FoodBoutiqueAPI } from './food-api';
 
-const products = new FoodBoutiqueAPI();
-const data = await products.getProducts();
-const productsObj = data.results;
-
+const products = {
+  page: 1,
+  perPage: 9,
+  totalPages: 60,
+  results: [
+    {
+      _id: '640c2dd963a319ea671e383b',
+      name: 'Ackee',
+      img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e383b.png',
+      category: 'Fresh_Produce',
+      price: 8.99,
+      size: '16 oz',
+      is10PercentOff: false,
+      popularity: 8,
+    },
+    {
+      _id: '640c2dd963a319ea671e3860',
+      name: 'Allspice',
+      img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3860.png',
+      category: 'Pantry_Items',
+      price: 2.99,
+      size: '1.5oz',
+      is10PercentOff: false,
+      popularity: 8,
+    },
+    {
+      _id: '640c2dd963a319ea671e3861',
+      name: 'Almond Extract',
+      img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3861.png',
+      category: 'Pantry_Items',
+      price: 4.99,
+      size: '2oz',
+      is10PercentOff: false,
+      popularity: 0,
+    },
+    {
+      _id: '640c2dd963a319ea671e385f',
+      name: 'Almond Milk',
+      img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e385f.png',
+      category: 'Beverages',
+      price: 3.99,
+      size: '1L',
+      is10PercentOff: false,
+      popularity: 0,
+    },
+    {
+      _id: '640c2dd963a319ea671e3814',
+      name: 'Almonds',
+      img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3814.png',
+      category: 'Pantry_Items',
+      price: 8.99,
+      size: '16 oz bag',
+      is10PercentOff: false,
+      popularity: 0,
+    },
+    {
+      _id: '640c2dd963a319ea671e385e',
+      name: 'Ancho Chillies',
+      img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e385e.png',
+      category: 'Pantry_Items',
+      price: 4.99,
+      size: '100g',
+      is10PercentOff: false,
+      popularity: 0,
+    },
+    {
+      _id: '640c2dd963a319ea671e3865',
+      name: 'Anchovy Fillet',
+      img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3865.png',
+      category: 'Meat_&_Seafood',
+      price: 3.49,
+      size: '2oz',
+      is10PercentOff: false,
+      popularity: 0,
+    },
+    {
+      _id: '640c2dd963a319ea671e3660',
+      name: 'Apple Cider Vinegar',
+      img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3660.png',
+      price: 6.99,
+      size: '500 ml',
+      category: 'Pantry_Items',
+      is10PercentOff: false,
+      popularity: 0,
+    },
+    {
+      _id: '640c2dd963a319ea671e3825',
+      name: 'Apples',
+      img: 'https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3825.png',
+      category: 'Fresh_Produce',
+      price: 1.5,
+      size: '1 lb',
+      is10PercentOff: true,
+      popularity: 0,
+    },
+  ],
+};
+const productsObj = products.results;
 const productCardList = document.querySelector('.product-card-list');
 productCardList.innerHTML = createProductsList(productsObj);
 
@@ -31,7 +125,7 @@ export function createProductsList(productsObj) {
                     <h3 class="price">$${price}</h3>
                     <button class="btn btn-shopping-cart js-add-btn">
                         <svg class="icon-shopping-cart" width="18" height="18">
-                            <use href="./img/icons.svg#icon-shopping-cart"></use>
+                            <use href="./icons.svg#icon-shopping-cart"></use>
                         </svg>
                     </button>
                 </div>
