929015f3994a263a6973a012f46577a8169a99d3
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-11-22 17:21:06 +0200

    finish styles

--- src/js/markup-product-list.js
+++ src/js/markup-product-list.js
@@ -23,11 +23,11 @@ export function createProductsList(productsObj) {
         return `<li class="products-card-item">
                 <img class="product-image" src="${img}" alt="${name}" />
                 <h3 class="product-name">${name}</h3>
-                <div class="wrap-procuct-text">
-                    <p class="procuct-text">Category:<b> ${category}</b></p>
-                    <p class="procuct-text">Size:<b> ${size}</b></p>
-                    <p class="procuct-text">Popularity:<b> ${popularity}</b></p>
-                </div>
+                    <ul class="procuct-description-list">
+                        <li><p class="procuct-text">Category:<b> ${category}</b></p></li>
+                        <li><p class="procuct-text">Size:<b> ${size}</b></p></li>
+                        <li><p class="procuct-text">Popularity:<b> ${popularity}</b></p></li>
+                    </ul>
                 <div class="wrap-prise-and-btn">
                     <h3 class="price">$${price}</h3>
                     <button class="btn btn-shopping-cart js-add-btn">
