2fa81768af1af244827db33a4c96a3b124b9938a
Sign-maker <muratovdv76@gmail.com>
2023-11-23 20:57:28 +0200

    Update cartHandler.js

--- src/js/cart/cartHandler.js
+++ src/js/cart/cartHandler.js
@@ -1,4 +1,4 @@
-import { Storage } from './local-storage-api';
+import { Storage } from '../local-storage-api';
 
 const SHOP_STORAGE = 'shop-storage';
 const shopStorage = new Storage(SHOP_STORAGE);
