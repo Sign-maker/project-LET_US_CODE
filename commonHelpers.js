ca35130bc30f7c4840778376a7ed48a795152cae
Sign-maker <136499347+Sign-maker@users.noreply.github.com>
2023-11-25 13:16:01 +0000

    Deploying to gh-pages from @ Sign-maker/project-LET_US_CODE@e29f533ba3be1dd9d13f0a7587b3cb93f4e11024 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,8 +1,8 @@
-import{S as n}from"./assets/local-storage-api-c31d5bee.js";const m="shop-storage";new n(m);function f(t){return t.map(({category:c,img:r,is10PercentOff:u,name:a,price:o,size:d,_id:g})=>`<li class="cart-item" data-id="${g}">
-      <img class="cart-img-product" src="${r}" alt="${a}" />
+import{S as i}from"./assets/local-storage-api-579d8235.js";const m="shop-storage";new i(m);const y=[{_id:"640c2dd963a319ea671e383b",name:"Ackee",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e383b.png",category:"Fresh_Produce",price:8.99,size:"16 oz",is10PercentOff:!1,popularity:8},{_id:"640c2dd963a319ea671e3860",name:"Allspice",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3860.png",category:"Pantry_Items",price:2.99,size:"1.5oz",is10PercentOff:!1,popularity:8},{_id:"640c2dd963a319ea671e3660",name:"Apple Cider Vinegar",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3660.png",price:6.99,size:"500 ml",category:"Pantry_Items",is10PercentOff:!1,popularity:0},{_id:"640c2dd963a319ea671e3861",name:"Almond Extract",img:"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e3861.png",category:"Pantry_Items",price:4.99,size:"2oz",is10PercentOff:!1,popularity:0}];function l(t){return t.map(({category:a,img:e,name:c,price:o,size:s,_id:u})=>`<li class="cart-item" data-id="${u}">
+      <img class="cart-img-product" src="${e}" alt="${c}" />
       <div class="cart-product-description">
-          <p class="cart-product-name">${a}</p>
-          <p class="cart-product-text">Category: <span class="cart-text-black gap">${c}</span>  Size: <span class="cart-text-black">${d}</span></p>
+          <p class="cart-product-name">${c}</p>
+          <p class="cart-product-text">Category: <span class="cart-text-black gap">${a}</span>  Size: <span class="cart-text-black">${s}</span></p>
           <p class="cart-product-price">$${o}</p>
       </div>
       <button type="button" class="cart-product-close-btn">
@@ -10,5 +10,5 @@ import{S as n}from"./assets/local-storage-api-c31d5bee.js";const m="shop-storage
               <use href="./icons.svg#icon-close"></use>
           </svg>
       </button>
-  </li>`).join("")}let e=[];const S=document.querySelector(".nav-span"),l=document.querySelector(".cart-list"),i=new n("shop-storage");l.addEventListener("click",b);function p(){JSON.parse(localStorage.getItem("shop-storage"));const t=[];t.push(i.getValue()),e=t,l.innerHTML=f(e),s(e),console.log("Total price:",s(e)??0),C(e.length)}p();function C(t){S.textContent=t}function s(t){if(t.length)return t.reduce((c,r)=>c+=r.price,0)}function b(t){if(!t.target.closest(".cart-product-close-btn"))return;const r=t.target.closest("li").dataset.id,a=(i.getValue()??[]).filter(o=>o._id!==r);localStorage.setItem("popularProducts",JSON.stringify(a)),p()}
+  </li>`).join("")}const f=document.querySelector(".cart-list");f.innerHTML=l(y);let r=[];const S=document.querySelector(".nav-span"),p=document.querySelector(".cart-list"),d=new i("shop-storage");p.addEventListener("click",C);function g(){JSON.parse(localStorage.getItem("shop-storage"));const t=[];t.push(d.getValue()),r=t,p.innerHTML=l(r),n(r),console.log("Total price:",n(r)??0),h(r.length)}g();function h(t){S.textContent=t}function n(t){if(t.length)return t.reduce((a,e)=>a+=e.price,0)}function C(t){if(!t.target.closest(".cart-product-close-btn"))return;const e=t.target.closest("li").dataset.id,o=(d.getValue()??[]).filter(s=>s._id!==e);localStorage.setItem("popularProducts",JSON.stringify(o)),g()}
 //# sourceMappingURL=commonHelpers.js.map
