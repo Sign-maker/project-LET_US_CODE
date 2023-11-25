27c483458c680a97ef40c825b8cd51a0403ce9e5
Tetiana85 <4215654@gmail.com>
2023-11-26 00:12:26 +0100

    response.data.message+css

--- src/js/subscription.js
+++ src/js/subscription.js
@@ -1,3 +1,104 @@
+// import { FoodBoutiqueAPI } from './food-api.js';
+// import Swal from 'sweetalert2';
+
+// const foodApi = new FoodBoutiqueAPI();
+
+// const form = document.querySelector('form[name="email_address"]');
+
+// form.addEventListener('submit', async function (event) {
+//   event.preventDefault();
+
+//   const emailInput = event.currentTarget.elements['footer-input-email'];
+//   const emailValue = emailInput.value;
+
+//   try {
+//     await foodApi.subscribeToNewsletter({ email: emailValue });
+//     emailInput.value = '';
+  
+//     Swal.fire({
+//       icon: 'success',
+//       title: 'Successfully subscribed!',
+//       showConfirmButton: true,
+//       confirmButtonColor: '#6D8434',
+//       showCancelButton: false,
+//       customClass: {
+//         popup: 'small-popup',
+//         title: 'custom-title',
+//         icon: 'custom-icon',
+
+//       },
+//     });
+//   } catch (error) {
+//     console.error('Error:', error);
+
+//     Swal.fire({
+//       icon: 'error',
+//       title: 'Error',
+//       text: 'There was an error subscribing. Please try again.',
+//       showConfirmButton: true,
+//       confirmButtonColor: '#6D8434',
+//       showCancelButton: false,
+//       customClass: {
+//         popup: 'small-popup',
+//         title: 'custom-title',
+//         icon: 'custom-icon',
+//       },
+//     });
+//   }
+// });
+
+// const foodApi = new FoodBoutiqueAPI();
+
+// const form = document.querySelector('form[name="email_address"]');
+
+// form.addEventListener('submit', async function (event) {
+//   event.preventDefault();
+
+//   const emailInput = event.currentTarget.elements['footer-input-email'];
+//   const emailValue = emailInput.value;
+
+//   try {
+//     await foodApi.subscribeToNewsletter({ email: emailValue });
+//     emailInput.value = '';
+  
+//     Swal.fire({
+//       icon: 'success',
+//       title: 'Successfully subscribed!',
+//       showConfirmButton: true,
+//       confirmButtonColor: '#6D8434',
+//       showCancelButton: false,
+//       customClass: {
+//         popup: 'small-popup',
+//         title: 'custom-title',
+//         icon: 'custom-icon',
+//       },
+//     });
+//   } catch (error) {
+//     console.error('Error:', error);
+
+//     let errorMessage = 'There was an error subscribing. Please try again.';
+
+//     if (error.response && error.response.data && error.response.data.message) {
+//       errorMessage = error.response.data.message;
+//     }
+
+//     Swal.fire({
+//       icon: 'error',
+//       // title: 'Error',
+//       title: errorMessage,
+//       showConfirmButton: true,
+//       confirmButtonColor: '#6D8434',
+//       showCancelButton: false,
+//       customClass: {
+//         popup: 'small-popup',
+//         title: 'custom-title',
+//         icon: 'custom-icon',
+//       },
+//     });
+//   }
+// });
+
+
 import { FoodBoutiqueAPI } from './food-api.js';
 import Swal from 'sweetalert2';
 
@@ -12,12 +113,17 @@ form.addEventListener('submit', async function (event) {
   const emailValue = emailInput.value;
 
   try {
-    await foodApi.subscribeToNewsletter({ email: emailValue });
+    const response = await foodApi.subscribeToNewsletter({ email: emailValue });
     emailInput.value = '';
-  
+    
+    let successMessage = 'Successfully subscribed!';
+    if (response && response.data && response.data.message) {
+      successMessage = response.data.message;
+    }
+
     Swal.fire({
       icon: 'success',
-      title: 'Successfully subscribed!',
+      title: successMessage,
       showConfirmButton: true,
       confirmButtonColor: '#6D8434',
       showCancelButton: false,
@@ -25,16 +131,21 @@ form.addEventListener('submit', async function (event) {
         popup: 'small-popup',
         title: 'custom-title',
         icon: 'custom-icon',
-
       },
     });
   } catch (error) {
     console.error('Error:', error);
 
+    let errorMessage = 'There was an error subscribing. Please try again.';
+
+    if (error.response && error.response.data && error.response.data.message) {
+      errorMessage = error.response.data.message;
+    }
+
     Swal.fire({
       icon: 'error',
       title: 'Error',
-      text: 'There was an error subscribing. Please try again.',
+      text: errorMessage,
       showConfirmButton: true,
       confirmButtonColor: '#6D8434',
       showCancelButton: false,
@@ -46,6 +157,4 @@ form.addEventListener('submit', async function (event) {
     });
   }
 });
-
-
 
